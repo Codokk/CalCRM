cc52d1369ced9b1d118ebb28c3c395110b48c581
Cody Krist <Codokk101@gmail.com>
2020-04-05 11:31:04 -0400

    Added A Setup Function
    Added a setup file to build the database and tables on whatever server it's installed on

--- db.php
+++ db.php
@@ -5,5 +5,14 @@ $ssid = session_id();
 $db_user = "root";
 $db_pass = '';
 $db_name = "calendarDatabase";
-$DB = new mysqli("localhost", $db_user, $db_pass, $db_name) or die($DB->connect_error);
+$DB = new mysqli("localhost", $db_user, $db_pass) or die($DB->connect_error);
+$sql = "select schema_name from information_schema.schemata where schema_name = 'calendardatabase'";
+$qry = $DB->query($sql);
+if($qry->num_rows == 1)
+{
+    $DB->query("USE ".$db_name);
+} else {
+    include("./setup.php");
+    exit();
+}
 ?>
